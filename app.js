91f8d768f4c825b243e0b5006fda65c9ca5b0abb
Rossini <daniel@ingadigital.com.br>
2019-07-26 14:34:24 -0300

    commit

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+var user = require('./user.js');
 
-var users = ['doido'];
+var usersVar = [];
+
+user.user = '';
 
 io.on('connection', (socket) => {
-    socket.on("logar", (user) => {
-        if(users.indexOf(user) == -1){
-            users.push(user);
+    socket.on("logar", (userString) => {
+        if(usersVar.indexOf(userString) == -1){
+            usersVar.push(userString);
+            user.user = userString;
             socket.emit("logar", true);
         }else{
             socket.emit('logar', false);
         }
+    });
+
+    socket.on("getUser", (data)=>{
+        socket.emit("getUser", user.user);
+    });
+
+    socket.on("getUsersList", (data)=>{
+        socket.emit("getUsersList", usersVar);
+    });
+
+    socket.on("submitMessage", (data)=>{
+        io.emit("submitMessage", data);
     });
 });
 
---
+++ user.js
@@ -0,0 +1,14 @@
+class User{
+
+    constructor(){ }
+
+    set user(user){
+        this.user = user;
+    }
+
+    get user(){
+        return this.user;
+    }
+}
+
+module.exports.User;
